0f7a6aba8d4bf579f2a9b5f781e58471206952ee
tabassumshawlin <tabassumshawlin65@gmail.com>
2025-07-08 17:08:47 +0600

    job card and detais pages added

--- src/Components/JobCard.jsx
+++ src/Components/JobCard.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const JobCard = () => {
     return (
-        <div className="carousel-item w-1/4">
-            <div className="card bg-base-100 image-full shadow-sm">
-                <figure>
-                    <img
-                        src="https://i.ibb.co/8LxfPPcK/tim-mossholder-GOMhu-Cj-O9w-unsplash.jpg"
-                        alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">Job Title</h2>
-                    <p>Job Description</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+       <div className="card bg-base-100 shadow-md border border-gray-200 hover:shadow-lg transition">
+        <div className="card-body">
+          <h2 className="card-title text-lg font-bold text-primary">
+            Asst Programmer
+          </h2>
+          <p className="text-sm text-gray-600">ABC Tech. Ltd. — Tech</p>
+          <p className="text-sm"><span className="font-semibold">Location:</span> Mirpur DOHS, Dhaka</p>
+          <p className="text-sm"><span className="font-semibold">Salary:</span> ~30k</p>
+          <p className="text-sm"><span className="font-semibold">Type:</span> Full-time (Remote)</p>
+          <p className="text-sm"><span className="font-semibold">Deadline:</span> 14 July 2025</p>
+          <div className="card-actions justify-end mt-4">
+            <Link to={"/see-details"} className="btn btn-outline btn-sm btn-primary">Show Details</Link>
+          </div>
         </div>
-    );
+      </div>
+  );
 };
 
 export default JobCard;
